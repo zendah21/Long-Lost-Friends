7f6a745d84b6ee54531b883e3cb2d7db8ea63e70
ahmad <ahmedzendah2@gmail.com>
2024-04-12 11:56:51 +0300

    main commit

--- main.py
+++ main.py
@@ -61,21 +61,37 @@ def A_search(the_map):
             queue.append(extended_path)
 
 
-def get_index_of(robot,the_map):
-    for row in range(len(the_map)):
-        if robot in the_map[row]:
-            return row,the_map[row].index('A')
+def get_index_of(robot, the_map):
+    for row_idx, row in enumerate(the_map):
+        if robot in row:
+            return row_idx, row.index(robot)
 
-def get_estimated_remaining_distance(current_index,target,the_map):
-    target_index=get_index_of(target,the_map)
-    return sum( target_index[0]-current_index[0] ,target_index[1]-current_index[1])
+def get_target_index(target, the_map):
+    return get_index_of(target, the_map)
+
+def get_current_index(sublist, the_map):
+    return get_index_of(sublist[-1], the_map)
+
+def get_estimated_remaining_distance(current_index, target_index):
+    return abs(target_index[0] - current_index[0]) + abs(target_index[1] - current_index[1])
+
+def calculate_estimated_distance(current_index, target_index):
+    return get_estimated_remaining_distance(current_index, target_index)
+
+def calculate_sorting_key(sublist, current_index, estimated_distance):
+    return sublist[-1] + estimated_distance
+
+def calculate_sublist_sorting_key(sublist, target, the_map):
+    target_index = get_target_index(target, the_map)
+    current_index = get_current_index(sublist, the_map)
+    estimated_distance = calculate_estimated_distance(current_index, target_index)
+    return calculate_sorting_key(sublist, current_index, estimated_distance)
+
+def get_sorted_queue(queue, target, the_map):
+    return sorted(queue, key=lambda sublist: calculate_sublist_sorting_key(sublist, target, the_map))
+
 
 if __name__ == '__main__':
 
     the_map = get_map(FILENAME)
     print(get_index_of('A',the_map))
-
-
-
-
-
